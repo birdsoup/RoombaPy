ba2d25de988d2d7d10d9fea52009ba5cb47cc11a
xPowerz <ccpowers@bu.edu>
2018-01-21 16:07:55 -0500

    testing changes

--- roomba.py
+++ roomba.py
@@ -56,9 +56,9 @@ class Roomba(object):
     def __init__(self, dd_pin=7, baud_rate=BAUD_RATES[0], time_out=3.0):
         self.dd_pin = dd_pin
 
-        port = serial.Serial("/dev/ttyAMA0", baudrate=baud_rate, timeout=time_out)
-        port.close()
-        port.open()
+        self.port = serial.Serial("/dev/ttyAMA0", baudrate=baud_rate, timeout=time_out)
+        self.port.close()
+        self.port.open()
 
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.dd_pin, GPIO.OUT)
@@ -72,57 +72,57 @@ class Roomba(object):
         time.sleep(2)
 
     def start(self):
-        turn_on()
-        write_num(128)
+        self.turn_on()
+        self.write_num(128)
 
     def set_mode(self, mode):
-        write_num(mode)
+        self.write_num(mode)
         time.sleep(1)
 
     def passive_mode(self):
-        start()
+        self.start()
 
     def safe_mode(self):
-        set_mode(SAFE)
+       self.set_mode(SAFE)
 
     def full_mode(self):
-        set_mode(FULL)
+        self.set_mode(FULL)
 
     def passive_mode(self):
-        set_mode(PASSIVE)
+        self.set_mode(PASSIVE)
 
     def set_digits(self, digit1, digit2, digit3, digit4):
-        safe_mode()
-        write_num(164)
+        self.safe_mode()
+        self.write_num(164)
         #todo, should probably convert this to write_num
-        port.write(digit1)
-        port.write(digit2)
-        port.write(digit3)
-        port.write(digit4)
+        self.port.write(digit1)
+        self.port.write(digit2)
+        self.port.write(digit3)
+        self.port.write(digit4)
 
     def set_digits_string(self, msg):
-        set_digits(msg[0], msg[1], msg[2], msg[3])
+        self.set_digits(msg[0], msg[1], msg[2], msg[3])
 
     #starts cleaning
     def cover(self):
-        turn_on()
-        write_num(135)
+        self.turn_on()
+        self.write_num(135)
 
     def cover_and_dock(self):
-        turn_on()
-        write_num(143)
+        self.turn_on()
+        self.write_num(143)
 
     def spot(self):
-        turn_on()
-        write_num(134)
+        self.turn_on()
+        self.write_num(134)
 
     def power(self):
-        write_num(133)
+        self.write_num(133)
 
     #I think this should change the baud rate, untested
     def change_baudrate(self):
-        power()
-        turn_on()
+        self.power()
+        self.turn_on()
         time.sleep(2)
         GPIO.output(self.dd_pin, GPIO.HIGH)
         time.sleep(0.250)
@@ -140,17 +140,17 @@ class Roomba(object):
         time.sleep(2)
 
     def set_baud_mode(self, mode):
-        write_num(129)
-        write_num()
-        sleep(0.100) #have to wait at least 100ms before sending new commands at the new baud rate
+        self.write_num(129)
+        self.write_num()
+        self.sleep(0.100) #have to wait at least 100ms before sending new commands at the new baud rate
         #todo should change the baud rate on the serial connection
 
     def set_baudrate(self, baudrate):
         #should throw exception if baudrate is invalid
-        set_baud_mode(self.BAUD_RATE_OPTIONS[baudrate])
+        self.set_baud_mode(self.BAUD_RATE_OPTIONS[baudrate])
 
     def run_demo(self, demo):
-        write_num(demo)
+        self.write_num(demo)
 
     def drive(self, velocity, radius):
         #todo have bounds and special cases
@@ -160,91 +160,91 @@ class Roomba(object):
         #Turn in place clockwise = hex FFFF
         #Turn in place counter-clockwise = hex 0001
 
-        write_num(137)
-        write_num((velocity >> 8) & 0xff)
-        write_num(velocity & 0xff)
-        write_num((radius >> 8) & 0xff)
-        write_num(radius & 0xff)
+        self.write_num(137)
+        self.write_num((velocity >> 8) & 0xff)
+        self.write_num(velocity & 0xff)
+        self.write_num((radius >> 8) & 0xff)
+        self.write_num(radius & 0xff)
 
     def drive_direct(self, right_velocity, left_velocity):
         #v: -500 to 500 m/s
-        write_num(145)
-        write_num((right_velocity >> 8) & 0xff)
-        write_num(right_velocity & 0xff)
-        write_num((left_velocity >> 8) & 0xff)
-        write_num(left_velocity & 0xff)
+        self.write_num(145)
+        self.write_num((right_velocity >> 8) & 0xff)
+        self.write_num(right_velocity & 0xff)
+        self.write_num((left_velocity >> 8) & 0xff)
+        self.write_num(left_velocity & 0xff)
 
     def set_LEDs(self, LED_bits, power_color, power_intensity):
-        write_num(139)
-        write_num(LED_bits)
-        write_num(power_color)
-        write_num(power_intensity)
+        self.write_num(139)
+        self.write_num(LED_bits)
+        self.write_num(power_color)
+        self.write_num(power_intensity)
 
         self.led_bits = LED_bits
         self.power_color = power_color
         self.power_intensity = power_intensity
 
     def set_power_LED(self, power_color, power_intensity):
-        set_LEDs(led_bits, power_color, power_intensity)
+        self.set_LEDs(led_bits, power_color, power_intensity)
 
     def set_advance_LED(self, led_bit): 
-        set_LEDS(led_bits & ((led_bit & 1) << 3), power_color, power_intensity)
+        self.set_LEDS(led_bits & ((led_bit & 1) << 3), power_color, power_intensity)
 
     def set_play_LED(self, led_bit): 
-        set_LEDS(led_bits & ((led_bit & 1) << 1), power_color, power_intensity)
+        self.set_LEDS(led_bits & ((led_bit & 1) << 1), power_color, power_intensity)
 
     #not entirely sure how this works, should add more about it later
     def pwm_low_side_drivers(self, duty_cycle_0, duty_cycle_1, duty_cycle_2):
         #max duty cycle is 128, multiply the percent of battery 
         #voltage that you want to use on the motor by the max duty cycle
         #todo should verify that it's in the right range (0-128)
-        write_num(144)
-        write_num(duty_cycle_2)
-        write_num(duty_cycle_1)
-        write_num(duty_cycle_0)
+        self.write_num(144)
+        self.write_num(duty_cycle_2)
+        self.write_num(duty_cycle_1)
+        self.write_num(duty_cycle_0)
 
     #can't control speed of motors with this method, should use pwm_low_side_drivers instead
     def low_side_drivers(self, low_side_driver_0, low_side_driver_1, side_driver):
-        write_num(138)
+        self.write_num(138)
         bits = (low_side_driver_0 & 1)
         bits |= (low_side_driver_1 & 1) << 1
         bits |= (side_driver & 1) << 2
-        write_num(bits)
+        self.write_num(bits)
 
     #not exactly sure what this really does
     def send_IR(self, value):
-        write_num(151)
-        write_num(value)
+        self.write_num(151)
+        self.write_num(value)
 
     # Scores are lists of tuples with each tuple being (note_num, note_duration)
     # with the first tuple indicating (song_number, song_length)
     sample_score = [(0, 3), (71, 64), (69, 64), (67, 64)]
     def write_song(score):
-        write_num(140)
-        write_num(score[0][0])
-        write_num(score[0][1])
+        self.write_num(140)
+        self.write_num(score[0][0])
+        self.write_num(score[0][1])
         for note in score[1:]:
-            write_num(note[0])
-            write_num(note[1])
+            self.write_num(note[0])
+            self.write_num(note[1])
 
     def play_song(self, song_number):
         #song_number 0-15
-        write_num(141)
-        write_num(song_number)
+        self.write_num(141)
+        self.write_num(song_number)
 
     def sensors(self, packet_id):
         #ids 0-42
-        write_num(142)
-        write_num(packet_id)
+        self.write_num(142)
+        self.write_num(packet_id)
         sleep(0.015) #sensors are only updated every 15ms
         #todo need to read the sensor data from the serial port
 
     def query_list(self, packet_ids):
         #ids 0-42
-        write_num(149)
-        write_num(len(packet_ids))
+        self.write_num(149)
+        self.write_num(len(packet_ids))
         for packet_id in packet_ids:
-            write_num(packet_id)
+            self.write_num(packet_id)
         sleep(0.015) #sensors are only updated every 15ms
         #todo need to read the sensor data back from the serial port
 
@@ -252,9 +252,9 @@ class Roomba(object):
         pass
 
     def write_num(self, num):
-        port.write(struct.pack('!B', num))
+        self.port.write(struct.pack('!B', num))
 
         #need to wait at least 200microseconds between characters when at 115200 baud rate 
-        if baudrate == 115200:
+        if self.baudrate == 115200:
             sleep(200e-9) 
 
